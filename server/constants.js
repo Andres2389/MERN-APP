c8e10349536a891c6d7b88b86d54db4f72095297
andres2389 <aframirez@soy.sena.edu.co>
2024-08-09 11:59:52 -0500

    nuevo aplicativo web hecho con MERN

---
+++ server/constants.js
@@ -0,0 +1,16 @@
+const DB_USER = "mern"
+const DB_PASSWORD ="Vale1521"
+const DB_HOST = "mern-web-personal.gjlwfej.mongodb.net"
+
+const API_VERSION ="v1"
+const IP_SERVER ="localhost"
+const JWT_SECRET_KEY = "kmfdgmps4s55sg6sgsgsgs6gsga8ffimswfmwiosfmwf"
+
+module.exports = {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  API_VERSION,
+  IP_SERVER,
+  JWT_SECRET_KEY
+}
